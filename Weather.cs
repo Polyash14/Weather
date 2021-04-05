5c4fee2c577550aa1963fc9a7ed8dcd423145de7
Polyash14 <polyafire14@gmail.com>
2021-04-06 01:26:52 +0300

    Revert "Добавила классы коллекции"
    This reverts commit 788837ff2a69cc6948531e0d97050530cf63a916.

--- Weather.cs
+++ Weather.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Weather
 {
@@ -75,9 +72,7 @@ namespace Weather
         }
 
        
+       
         
-       
-       
-
     }
 }
